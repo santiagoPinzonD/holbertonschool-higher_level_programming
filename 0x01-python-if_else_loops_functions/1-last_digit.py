ff39ff79c4a5370166c8e37f8b866e0d14e7ad09
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-01 20:14:10 +0000

    error de sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ if number > 0:
     digit = number % 10
 if number < 0:
     digit = (number * -1) % 10
+    digit = digit * -1
 print("Last digit of {} is {}" .format(number, digit), end="")
 if digit > 5:
     print(" and is greater than 5")
