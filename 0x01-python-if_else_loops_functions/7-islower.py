26512de334d5735c7d127dd3dcaa9b9a80bb6453
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-01 22:45:13 +0000

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def islower(c):
+    for x in range(97, 122):
+        if ord(c) == x:
+            return True
+    return False
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
