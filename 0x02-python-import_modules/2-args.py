f4099b9e787c7d90f4106c6513a0ab8d0458a075
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-03 22:29:50 +0000

    error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ def main():
     import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
+    if len(sys.argv) == 2:
+        print("1 argument:")
     else:
         print("{} argument:" .format(len(sys.argv)))
     for x in range(1, len(sys.argv)):
