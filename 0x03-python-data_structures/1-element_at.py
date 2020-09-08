ac16289b6ec1d305ab728409a005f6d7b7beb43d
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-08 19:06:30 +0000

    python

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return
+    if idx > len(my_list):
+        return
+    for x in range(0, len(my_list)):
+        return(my_list[idx])
