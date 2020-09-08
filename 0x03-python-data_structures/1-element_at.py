34e3ab7171617f427f2be5eee22bd924b21e1e32
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-08 19:21:32 +0000

    erro of condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return
     for x in range(0, len(my_list)):
         return(my_list[idx])
