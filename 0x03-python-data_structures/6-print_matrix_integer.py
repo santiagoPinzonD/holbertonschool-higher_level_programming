63e34cb04e9022b1c211acf3a067dfd70d118a09
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-09 00:55:48 +0000

    print

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if(matrix == [[]]):
+        print("")
+    else:
+        for i in matrix:
+            z = 0
+            for x in i:
+                if z >= len(i) - 1:
+                    print("{:d}".format(x), end="")
+                else:
+                    print("{:d}".format(x), end=" ")
+            print("")
