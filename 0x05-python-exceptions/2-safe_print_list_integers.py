957a3f33358788646a6bc5816e40decd05d9221b
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-22 03:42:01 +0000

    jump of line

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     suma = 0
-    for z in range(0, x):
+    for z in range(x):
         try:
             print("{:d}".format(my_list[z]), end="")
             suma += 1
         except (TypeError, ValueError):
             pass
+    print()
     return suma
