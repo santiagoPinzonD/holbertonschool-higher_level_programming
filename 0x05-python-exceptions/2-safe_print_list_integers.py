686567832725b37fc5a553e1d459438eed5de3c4
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-22 03:24:21 +0000

    error 404

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for z in range(0, x):
             if type(my_list[z]) is int:
-                print("{:d}" .format(my_list[z]), end="")
+                print("{:d}".format(my_list[z]), end="")
                 suma += 1
-        print()
-        return suma
     except (TypeError, ValueError):
-        print()
-        return z
+        pass
+    return suma
