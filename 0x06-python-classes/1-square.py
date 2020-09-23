75c2ddd5b3c73f6f72979674d3a772f57213e3d1
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-23 00:45:01 +0000

    private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class that define a size private"""
+
+
+class Square:
+    """Class"""
+    __size = 0
+    def __init__(self, size):
+        """Private convert"""
+        self.__size = size
