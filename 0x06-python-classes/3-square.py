cc6742beb9d0f8d4820b38cd4e1cae2a6f4f98eb
santiagopinzonD <santiagopinzon456@gmail.com>
2020-09-23 01:14:58 +0000

    error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Class that define as the point zero"""
+"""funtion that define the area"""
 
 
 class Square:
-    """Class square"""
+    """Class"""
     __size = 0
 
     def __init__(self, size=0):
-        """Private size"""
+        """Private"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -15,5 +15,5 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        """Returns the current square area"""
+        """Return area to square"""
     return self.__size ** 2
