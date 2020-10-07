92b1091360ee9007fe74165c18623253fce18ed4
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-07 21:59:44 +0000

    close file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """reading a file"""
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read() end="")
+        print(myfile.read(), end="")
     myfile.close()
