89f5e5729220eeda910b21f17d954ae920dee5bd
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-07 04:37:32 +0000

    error

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """create"""
 
+
 class MyInt(int):
     """create function"""
 
     def __eq__(self, other):
         """eq comparation"""
         return int.__ne__(self, other)
-   
+
     def __ne__(self, other):
         """ne comparation"""
         return int.__eq__(self, other)
