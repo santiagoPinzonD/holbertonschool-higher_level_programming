ec114c128a26c01c3a57aa3cde11ebabb348cf1e
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-07 04:36:24 +0000

    error of pep8

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,7 +7,7 @@ class MyInt(int):
     def __eq__(self, other):
         """eq comparation"""
         return int.__ne__(self, other)
-    
+   
     def __ne__(self, other):
         """ne comparation"""
         return int.__eq__(self, other)
