3cf0d8aedbfedf8527c9aabdcad87c7b491d0db2
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-07 00:34:05 +0000

    changes

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,12 +18,14 @@ class BaseGeometry():
 
 class Rectangle(BaseGeometry):
     """create a class"""
+
     def __init__(self, width, height):
         """init of function"""
-        self.__width = width
-        self.__height = height
+
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
 
     def area(self):
         """create a function of area"""
