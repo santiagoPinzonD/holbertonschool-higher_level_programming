4f22faa5e40b7e5c781a2226e83314a10057a574
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-08 04:20:37 +0000

    estudent

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""create a class"""
+
+
+class Student():
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """tha same that 10"""
+        list1 = {}
+        if attrs:
+                for k, v in self.__dict__.items():
+                    for x in attrs:
+                        if x == k:
+                            list1[k] = v
+                return list1
+        return self.__dict__
