aabcb62063005163768ef94c2bf27b8c15cb72af
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-08 02:56:26 +0000

    error of comments

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """create"""
-
-
 import sys
 import os.path as path
+
+
 save1 = __import__('7-save_to_json_file').save_to_json_file
 load1 = __import__('8-load_from_json_file').load_from_json_file
 
