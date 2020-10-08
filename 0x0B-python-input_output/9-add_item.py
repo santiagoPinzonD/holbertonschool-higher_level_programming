2ebc9efc230e6b4c711c18b9e775a1b8274ac859
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-08 03:01:06 +0000

    recrazy

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,11 +8,9 @@ save1 = __import__('7-save_to_json_file').save_to_json_file
 load1 = __import__('8-load_from_json_file').load_from_json_file
 
 
-if __name__ == '__main__':
-    """create a function"""
-    list1 = []
-    args = sys.argv[1:]
+list1 = []
+args = sys.argv[1:]
 
-    if path.exists("add_item.json"):
-        list1 = load1("add_item.json")  # load return a list
-    save1(list1 + args, "add_item.json")  # toma una list y la serializa
+if path.exists("add_item.json"):
+    list1 = load1("add_item.json")  # load return a list
+save1(list1 + args, "add_item.json")  # toma una list y la serializa
