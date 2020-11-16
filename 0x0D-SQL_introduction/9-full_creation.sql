c14fc434d98f70392663fd05a6c5726f3086fcdb
santiagopinzonD <santiagopinzon456@gmail.com>
2020-11-16 21:50:57 +0000

    error of name

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,11 @@
 -- create second table and insert values
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    `name` name VARCHAR(256)
+    score INT
+);
 INSERT INTO second_table
 VALUES (1, "Juan", 10),
-      (2, "Alex", 3),
-      (3, "Bob", 14),
-      (4, "George", 8);
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
