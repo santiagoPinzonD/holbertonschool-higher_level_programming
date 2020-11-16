c87bced2fd45968493cf4e899f07ff39956f9913
santiagopinzonD <santiagopinzon456@gmail.com>
2020-11-16 21:46:09 +0000

    full creation of a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create second table and insert values
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
 VALUES (1, "Juan", 10),
       (2, "Alex", 3),
