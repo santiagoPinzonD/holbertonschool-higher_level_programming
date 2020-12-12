e18a914d52c2ecf7846c4cd0a61df4487f31273b
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-12 20:43:28 +0000

    add binary upper and lower case

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host=lc, port=3306, user=userr, passwd=passw, db=namd)
     cur = db.cursor()
-    exe = cur.execute("SELECT states.id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    exe = cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id;")
     for x in cur.fetchall():
         print(x)
     cur.close()
