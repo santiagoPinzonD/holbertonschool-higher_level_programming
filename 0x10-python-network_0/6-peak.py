f2d615b30700b6fdd1f22fbf27a36aaacae295f2
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-19 01:55:29 +0000

    try 1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,4 +5,5 @@
 def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
-    return (max(list_of_integers))
+    list_of_integers.sort()
+    return (list_of_integers[-1])
