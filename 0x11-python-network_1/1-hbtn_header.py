e648a6bb0d5f35ec6fd91ff485d70d2a4a846326
santiagopinzonD <santiagopinzon456@gmail.com>
2021-01-11 21:24:54 +0000

    error of import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,9 @@
 """ script that takes in a URL, sends a request to the
 URL and displays the value of the X-Request-Id
 """
+from urllib.request import urlopen
+import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
