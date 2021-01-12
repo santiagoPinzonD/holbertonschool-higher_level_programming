dd8b202b54e5511a8e67e6a2dfb47248e8919eb9
santiagopinzonD <santiagopinzon456@gmail.com>
2021-01-12 17:30:30 +0000

    task late

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that takes your Github credentials (username and password)
+and uses the Github API to display your id """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(user, passw))
+    d = r.json()
+    print("{}".format(d.get("id")))
