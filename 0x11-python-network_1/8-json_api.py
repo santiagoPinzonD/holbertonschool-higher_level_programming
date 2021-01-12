4535bc7298d260d2dcb78073c6d65d065c50815b
santiagopinzonD <santiagopinzon456@gmail.com>
2021-01-12 01:29:06 +0000

    error

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        d = r.json()
+        if d == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(d.get("id"), d.get("name")))
+    except:
+        print("Not a valid JSON")
