cbcab8c65e0f6bcf6da69cfb113491bc58cf3810
santiagopinzonD <santiagopinzon456@gmail.com>
2021-02-03 00:34:17 +0000

    cript that prints x times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+let i;
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+}
+for (i = 0; i < x; i++) {
+  console.log('C is fun');
+}
