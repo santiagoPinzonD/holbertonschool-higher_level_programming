982fc586350dd323dbdc4e64688df0e1868766ba
santiagopinzonD <santiagopinzon456@gmail.com>
2021-02-11 00:46:14 +0000

    fix and add coditions

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 $('#toggle_header').click(function () {
+  if (!$('header').hasClass('red', "green"))
+    {
+      $('header').addClass('green');
+    }
   if ($('header').hasClass('red')) {
     $('header').removeClass('red');
     $('header').addClass('green');
